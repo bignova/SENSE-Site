1de3d8ed9c7e2089b1d82dde539255a62ef86f43
zhanyi <jaredtan11221@gmail.com>
2018-05-04 14:04:01 -0400

    add navbar, logo

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '008877'
+app.config['MYSQL_PASSWORD'] = 'tzy123'
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
